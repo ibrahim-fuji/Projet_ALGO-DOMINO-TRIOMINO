0bfd922f6f0ca2d3141f0f079717cefaf770a4fb
Ibrahim El Bouhali <ibrahimokun0022@gmail.com>
2025-01-12 12:30:50 +0100

    Mise a jours de domino.h

--- domino.h
+++ domino.h
@@ -1,36 +1,33 @@
 #ifndef DOMINO_H
 #define DOMINO_H
 
-#include <stdbool.h>
-
-#define total_piece 28 // Nombre total de pièces dans un jeu de dominos
-
-typedef struct Domino {
-    int v_gauche;
-    int v_droite;
+// Définition de la structure Domino
+typedef struct {
+    int v_gauche; // Valeur gauche
+    int v_droite; // Valeur droite
 } Domino;
 
+// Structure pour les nœuds de la liste chaînée de dominos (pioche ou main de joueur)
 typedef struct DominoNode {
-    Domino domino_current;
-    struct DominoNode *domino_suivant;
+    Domino domino_current;         // Domino actuel
+    struct DominoNode* domino_suivant; // Pointeur vers le domino suivant
 } DominoNode;
 
+// Structure pour représenter une chaîne de dominos sur le plateau
 typedef struct Plateau {
-    Domino domino;
-    struct Plateau *precedent;
-    struct Plateau *suivant;
+    Domino domino;         // Domino actuel
+    struct Plateau* suivant; // Pointeur vers le domino suivant sur le plateau
 } Plateau;
 
+// Prototypes des fonctions
 DominoNode* initialiserDomino();
 Plateau* initialiserPlateau();
-DominoNode* supprimerDomino(DominoNode *tete, int v_gauche, int v_droite);
-void afficherDomino(DominoNode *tete);
-void afficherPlateau(Plateau *plateau);
-
-void init_pioche(Domino pioche[]);
-void melange_pioche(Domino pioche[]);
-void distribuer_piece(Domino pioche[], int* cpt_pioche, DominoNode** joueurs, int nb_joueurs);
+DominoNode* supprimerDomino(DominoNode* tete, int v_gauche, int v_droite);
+void afficherDomino(DominoNode* tete);
+void afficherPlateau(Plateau* plateau);
+DominoNode* melange_pioche(DominoNode* tete);
+void distribuer_piece(DominoNode** pioche, DominoNode** joueurs, int nb_joueurs);
 void afficherchaineDomino(DominoNode* chaine);
 void lib_chaine(DominoNode* chaine);
 
-#endif
+#endif // DOMINO_H
