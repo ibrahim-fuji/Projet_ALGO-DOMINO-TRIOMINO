3df5f4582de71356c7018aa93410c6ac68447f17
Abdou-adm <ser.abdellah80@gmail.com>
2025-01-12 11:44:52 -0500

    Ajout des fichiers pour dessiner des dominos

---
+++ domino.c
@@ -0,0 +1,89 @@
+// domino.c
+#include "domino.h"
+#include <math.h>
+
+// Function to draw a custom button and handle interactions
+bool DrawButtonCustom(Button *button) {
+    Vector2 mousePoint = GetMousePosition();
+    button->isHovered = CheckCollisionPointRec(mousePoint, button->rect);
+
+    Color currentColor = button->isHovered ? button->hoverColor : button->baseColor;
+    DrawRectangleRec(button->rect, currentColor);
+    DrawRectangleLinesEx(button->rect, 2, BLACK_TEXT);
+
+    int fontSize = 20;
+    int textWidth = MeasureText(button->text, fontSize);
+    Vector2 textPosition = {
+        button->rect.x + (button->rect.width - textWidth) / 2,
+        button->rect.y + (button->rect.height - fontSize) / 2
+    };
+
+    DrawText(button->text, textPosition.x, textPosition.y, fontSize, button->textColor);
+
+    // Return true if the button is clicked
+    return button->isHovered && IsMouseButtonPressed(MOUSE_BUTTON_LEFT);
+}
+
+// Function to draw a domino dot
+void DrawDominoDot(float x, float y) {
+    DrawCircle(x, y, DOTS_RADIUS, BLACK_TEXT);
+}
+
+// Function to draw the dots on a domino face
+void DrawDominoFace(float x, float y, int value, float width, float height) {
+    switch(value) {
+        case 1:
+            DrawDominoDot(x + width/2, y + height/2);
+            break;
+        case 2:
+            DrawDominoDot(x + width/4, y + height/4);
+            DrawDominoDot(x + 3*width/4, y + 3*height/4);
+            break;
+        case 3:
+            DrawDominoDot(x + width/4, y + height/4);
+            DrawDominoDot(x + width/2, y + height/2);
+            DrawDominoDot(x + 3*width/4, y + 3*height/4);
+            break;
+        case 4:
+            DrawDominoDot(x + width/4, y + height/4);
+            DrawDominoDot(x + 3*width/4, y + height/4);
+            DrawDominoDot(x + width/4, y + 3*height/4);
+            DrawDominoDot(x + 3*width/4, y + 3*height/4);
+            break;
+        case 5:
+            DrawDominoDot(x + width/4, y + height/4);
+            DrawDominoDot(x + 3*width/4, y + height/4);
+            DrawDominoDot(x + width/2, y + height/2);
+            DrawDominoDot(x + width/4, y + 3*height/4);
+            DrawDominoDot(x + 3*width/4, y + 3*height/4);
+            break;
+        case 6:
+            DrawDominoDot(x + width/4, y + height/4);
+            DrawDominoDot(x + width/4, y + height/2);
+            DrawDominoDot(x + width/4, y + 3*height/4);
+            DrawDominoDot(x + 3*width/4, y + height/4);
+            DrawDominoDot(x + 3*width/4, y + height/2);
+            DrawDominoDot(x + 3*width/4, y + 3*height/4);
+            break;
+    }
+}
+
+// Function to draw a domino
+void DrawDomino(Domino domino) {
+    float halfWidth = DOMINO_WIDTH / 2.0f;
+    float halfHeight = DOMINO_HEIGHT / 2.0f;
+    
+    // Draw the white background of the domino
+    DrawRectangle(domino.position.x, domino.position.y, DOMINO_WIDTH, DOMINO_HEIGHT, WHITE_COLOR);
+    
+    // Draw the separating line
+    DrawLine(domino.position.x + halfWidth, domino.position.y, 
+             domino.position.x + halfWidth, domino.position.y + DOMINO_HEIGHT, BLACK_TEXT);
+    
+    // Draw the outline
+    DrawRectangleLines(domino.position.x, domino.position.y, DOMINO_WIDTH, DOMINO_HEIGHT, BLACK_TEXT);
+    
+    // Draw the dots
+    DrawDominoFace(domino.position.x, domino.position.y, domino.top, halfWidth, DOMINO_HEIGHT);
+    DrawDominoFace(domino.position.x + halfWidth, domino.position.y, domino.bottom, halfWidth, DOMINO_HEIGHT);
+}
---
+++ domino.h
@@ -0,0 +1,42 @@
+// graphics.h
+#ifndef GRAPHICS_H
+#define GRAPHICS_H
+
+#include "game_state.h"    // Inclure game_state.h pour les définitions des structures et constantes
+#include "raymath.h"       // Inclusion nécessaire pour les fonctions Vector2
+#include "triomino.h"      // Inclusion du module Triomino
+
+// Définition des couleurs personnalisées
+#define CUSTOM_BLUE        (Color){0, 70, 140, 255}
+#define CUSTOM_GREEN       (Color){34, 139, 34, 255}
+#define CUSTOM_LIGHT_BLUE  (Color){135, 206, 250, 255}
+#define CUSTOM_RED         (Color){220, 20, 60, 255}
+#define CUSTOM_DARK_BLUE   (Color){0, 0, 139, 255}
+#define CUSTOM_BEIGE       (Color){245, 245, 220, 255}
+#define BLACK_TEXT         (Color){0, 0, 0, 255}
+
+// Définition de DARKRED si elle n'est pas déjà définie
+#ifndef DARKRED
+    #define DARKRED   (Color){139, 0, 0, 255}
+#endif
+
+// **Définition de WHITE_COLOR**
+#define WHITE_COLOR        (Color){255, 255, 255, 255}
+
+// Structure pour représenter un bouton
+typedef struct {
+    Rectangle rect;
+    const char *text;
+    Color baseColor;
+    Color hoverColor;
+    Color textColor;
+    bool isHovered;
+} Button;
+
+// Prototypes des fonctions de dessin graphiques générales
+bool DrawButtonCustom(Button *button);
+void DrawDominoDot(float x, float y);
+void DrawDominoFace(float x, float y, int value, float width, float height);
+void DrawDomino(Domino domino);
+
+#endif // GRAPHICS_H
